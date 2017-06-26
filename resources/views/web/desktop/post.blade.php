6fc14ca41ef46f2ece03024aecb9973370f0b3d0
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-06-26 09:53:24 +0700

    ok

--- resources/views/web/desktop/post.blade.php
+++ resources/views/web/desktop/post.blade.php
@@ -215,6 +215,20 @@
 				<div class="fb-like" data-href="" data-layout="standard" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>
 				<div class="fb-comments" data-href="{{ MyAPI::getUrlPostObj($data['post']) }}" data-numposts="5" data-width="100%"></div>
 			</div>
+			<script>
+				$(document).ready(function() {
+					setTimeout(function(){
+						toastr8.windows({
+							message:'Việc tìm kiếm sẽ nhanh hơn khi bạn trò chuyện với <a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en">bác sĩ tư vấn</a> !', 
+							title:"Mẹo nhỏ",
+							iconClass: "fa fa-user-md",
+							imgURI: ["{{ asset('public/images/desktop/logo-header.png') }}"],
+							timeOut:0,
+							positionClass:'toast8-bottom-left',
+						});
+					}, 60000);
+				});
+			</script>
 		</div>
 		<div class="flex0col2">
 			@include('web.desktop.slidebar')
